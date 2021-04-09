704a3990da4ef03f89165a0e3eab17529e6177c3
Momoe-211 <80560284+Momoe-211@users.noreply.github.com>
2021-04-09 10:07:43 +0900

    js課題08-1

---
+++ ex08-1/ex08-1.js
@@ -0,0 +1,6 @@
+const kaimono = document.getElementById("kaimono");
+console.log(`${kaimono.children[1].children[0].textContent}`);
+
+const todo = document.getElementById("todo");
+console.log(`${todo.children[0].textContent}`);
+console.log(`${todo.children[1].children[1].textContent}`);
