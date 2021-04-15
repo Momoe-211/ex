ccdd206d24b9130bdd1ebdeb4516bf9efa54ec09
Momoe-211 <80560284+Momoe-211@users.noreply.github.com>
2021-04-15 16:37:34 +0900

    js課題09-1

---
+++ ex09-1/ex09-1.js
@@ -0,0 +1,9 @@
+const add = document.getElementById("add");
+const remove = document.getElementById("remove");
+
+add.addEventListener("click",)
+
+remove.addEventListener("click", (event) => {
+    const last = ul.lastElementChild;
+    ul.removeElement(last);
+})
